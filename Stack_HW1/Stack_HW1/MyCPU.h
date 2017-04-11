0c2da5bd411830a6bc5aecc176ebce93d006b63d
Pasha2La <Pasha2La71@gmail.com>
2017-04-12 01:38:15 +0300

    started to write CPU

--- Stack_HW1/Stack_HW1/MyCPU.h
+++ Stack_HW1/Stack_HW1/MyCPU.h
@@ -10,6 +10,7 @@
 #include"Array.h"
 #include"MyException.h"
 #include"SmartPointers.h"
+#include"Header.h"
 
 class MyCPU
 {
@@ -17,15 +18,12 @@ public:
 	MyCPU();
 	void SetRegister(int data, int index);
 	int GetRerister(int index);
-	void SetLabel(int data, int index);
-	int GetLabel(int index);
+	void Execute(char *name);
 private:
 	//! Registers for CPU
 	int register_[100];
 	//! Stack for CPU
 	shared_ptr<Stack<int> > stack_;
-	//! Vector of labels for jumps
-	shared_ptr<Vector<int> > label_;
 	//! Stack of calls
 	shared_ptr<Stack<int> > callstack_;
 };
@@ -38,34 +36,86 @@ MyCPU::MyCPU()
 		register_[i] = 0;
 	}
 	stack_ = new Stack<int>;
-	label_ = new Vector<int>;
 	callstack_ = new Stack<int>;
 }
 
 void MyCPU::SetRegister(int data, int index)
 {
 	if (index < 0 || index>99)
-		throw MyException(1, "Bad index!", __FILE__, __LINE__);		//так??
+		throw MyException(1, "Bad index!", __FILE__, __LINE__);
 	register_[index] = data;
 }
 
 int MyCPU::GetRerister(int index)
 {
 	if (index < 0 || index>99)
-		throw MyException(1, "Bad index!", __FILE__, __LINE__);		//так??
+		throw MyException(1, "Bad index!", __FILE__, __LINE__);
 	return register_[index];
 }
 
-void MyCPU::SetLabel(int data, int index)
+void MyCPU::Execute(char *name)
 {
-	if (index < 0 || index>=label_->Size())
-		throw MyException(1, "Bad index!", __FILE__, __LINE__);		//так??
-	label_[index] = data;
-}
+	std::ifstream fin(name);
+	if (!fin.is_open())
+	{
+		throw MyException(2, "Cannot open file", __FILE__, __LINE__);
+	}
+	std::string buff;
+	while (!fin.eof())
+	{
+		std::getline(fin, buff);
+		int index = atoi(buff.substr(1).c_str());
+		switch (buff[0])
+		{
+		case '0':	//push for int
+			stack_->Push(index);
+			break;
+		case '1':	//push for reg
+			register_[index] = stack_->Top();
+			stack_->Pop();
+			break;
+		case '2':	//pop for reg
+			stack_->Push(register_[index]);
+			break;
+		case '3':	//add
+			int a = stack_->Top();
+			stack_->Pop();
+			int b = stack_->Top();
+			stack_->Pop();
+			stack_->Push(a+b);
+			break;
+		case '4':	//sub
+			int a = stack_->Top();
+			stack_->Pop();
+			int b = stack_->Top();
+			stack_->Pop();
+			stack_->Push(b - a);	//a-b or b-a?????????????
+			break;
+		case '5':
 
-int MyCPU::GetLabel(int index)
-{
-	if (index < 0 || index >= label_->Size())
-		throw MyException(1, "Bad index!", __FILE__, __LINE__);		//так??
-	return label_[index];
+		case '6':
+
+		case '7':
+
+		case '8':
+
+		case '9':
+
+		case 'a':
+
+		case 'b':
+
+		case 'c':
+
+		case 'd':
+
+		case 'e':
+
+		case 'f':
+
+		default:
+			break;
+		}
+	}
+
 }
